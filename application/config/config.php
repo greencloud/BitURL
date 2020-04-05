6fb4188cae242308c86b8417546ad9a9e7e2fda9
Rasmus van Guido <greencloud@users.noreply.github.com>
2020-04-05 14:00:39 +0800

    Create config.php

---
+++ application/config/config.php
@@ -0,0 +1,23 @@
+<?php
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+$config['base_url']				= 'https://biturl.me/';
+$config['index_page']			= '';
+$config['uri_protocol']			= 'REQUEST_URI';
+$config['charset']				= 'UTF-8';
+$config['subclass_prefix'] 		= 'MY_';
+$config['permitted_uri_chars']	= 'a-z 0-9~%.:_\-';
+$config['log_threshold'] 		= 1;
+$config['log_path']				= '';
+$config['log_file_permissions']	= 0644;
+$config['log_date_format']		= 'Y-m-d H:i:s';
+$config['error_views_path']		= '';
+$config['cache_path'] 			= '';
+$config['cache_query_string']	= FALSE;
+$config['cookie_prefix']		= 'biturl';
+$config['cookie_domain']		= '';
+$config['cookie_path']			= '/';
+$config['cookie_secure']		= FALSE;
+$config['cookie_httponly'] 		= FALSE;
+$config['compress_output']		= TRUE;
