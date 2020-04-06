f3ea20db1b2c5a530ce55c55d3fa3cd632ca9191
Rasmus van Guido <greencloud@users.noreply.github.com>
2020-04-07 00:17:59 +0800

    Update home.php

--- application/views/home.php
+++ application/views/home.php
@@ -20,4 +20,104 @@
 
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-exit($bluff);
+?><!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+
+    <title><?= $this->lang->line('meta_title');?></title>
+    <meta name="description" content="<?= $this->lang->line('meta_description');?>" />
+    <meta name="robots" content="index,follow" />
+    <meta name="author" content="Rasmus van Guido (genx.social/u/greencloud)" />
+
+    <link href="//fonts.googleapis.com/css?family=Montserrat:400,500,600,700" rel="stylesheet" />
+    <link href="//cdn-sttc1.srbts.me/img/favicon.ico?i=eb6ba0d5" rel="shortcut icon" type="image/x-icon" />
+    <link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" />
+    <link href="//fa.srbts.me/fontawesome/css/all.min.css" rel="stylesheet" />
+    <link href="<?= base_url();?>assets/biturl.css" rel="stylesheet" />
+
+    <script src="//code.jquery.com/jquery-2.2.4.min.js"></script>
+    <script src="//fa.srbts.me/fontawesome/js/all.min.js"></script>
+
+</head>
+<body>
+<div class="wrapper">
+    <div class="header">
+        <h1><?= $this->lang->line('page_title');?></h1>
+        <p><?= $this->lang->line('page_slogan');?></p>
+    </div>
+    <div class="content">
+        <form name="biturl" id="biturl" action="<?= base_url();?>biturl/input/" method="post">
+        <div class="form-group">
+            <label for="btinput"><?= $this->lang->line('form_label');?>:</label>
+            <input type="text" name="btinput" class="form-control btinput" id="btinput" value="" placeholder="<?= $this->lang->line('form_placeholder');?>" required />
+            <input type="submit" value="<?= $this->lang->line('form_submit_btn');?>" class="btn btn-primary" />
+            <input type="reset" value="<?= $this->lang->line('form_reset_btn');?>" class="btn btn-dark" />
+            <span id="loader"></span>
+        </div>
+        </form>
+        <div>
+            <hr />
+            <p><strong><?= sprintf($this->lang->line('ajax_header'), $this->lang->line('page_title_plain'));?>: &nbsp;</strong>
+                <input id="result" class="result" value="" placeholder="<?= $this->lang->line('ajax_result_default');?>" />
+                &nbsp;
+                <span class="far fa-copy copyurl" id="copyurl" title="<?= $this->lang->line('ajax_copy_title');?>"></span>
+                <span class="copymsg"></span></p>
+        </div>
+    </div>
+    <div class="footer">
+        <hr />
+        <h4><?= $this->lang->line('api_header');?></h4>
+        <p><?= $this->lang->line('api_notice');?></p>
+        <hr />
+        <footer>
+            <div>
+                <span><?= sprintf($this->lang->line('copyright'), date('Y'), $this->lang->line('page_title_plain'));?></span>
+            </div>
+        </footer>
+    </div>
+<script>
+  $(document).ready(function(){
+
+    $('#biturl').on('submit', function(e){
+      e.preventDefault();
+
+      var inputurl = $('#btinput').val();
+
+      $('#loader').empty();
+
+      if ( inputurl.length > 5 ) {
+        $.ajax({
+          type: 'post',
+          url: '<?= base_url();?>biturl/input/',
+          data: $('#biturl').serialize(),
+          dataType: 'json',
+          cache: false,
+          beforeSend: function() {
+            $('#loader').html('<img src="assets/loader.gif" />');
+          },
+          success: function(json) {
+            $('#loader').html('');
+
+            if (json['error']) {
+              $('#result').attr('value', json['error_msg']).css('color', '#db504a');
+            } else {
+              $('#result').attr('value', json['output_url']).css('color', '#254441');
+            }
+          },
+          error: function(xhr, ajaxOptions, thrownError) {
+            alert(thrownError + "\r\n" + xhr.statusText + "\r\n" + xhr.responseText);
+          }
+        });
+      }
+    });
+
+    $('#copyurl').on('click', function(){
+      //...
+    });
+  });
+</script>
+</div>
+</body>
+</html>
