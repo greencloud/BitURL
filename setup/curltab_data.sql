fda6b214be62305a1f9761d5ea303dd00a120009
Rasmus van Guido <greencloud@users.noreply.github.com>
2020-04-07 12:25:47 +0800

    Update and rename biturl_data.sql to curltab_data.sql

--- setup/curltab_data.sql
+++ setup/curltab_data.sql
@@ -3,18 +3,19 @@ SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
-CREATE TABLE `biturl_data` (
+CREATE TABLE `curltab_data` (
   `id` int(9) NOT NULL,
   `url_id` varchar(8) NOT NULL,
-  `url_actual` mediumtext NOT NULL,
+  `url_title` varchar(64) NOT NULL,
+  `url_long` mediumtext NOT NULL,
   `url_short` varchar(64) NOT NULL,
   `url_hits` int(9) NOT NULL DEFAULT '0',
-  `url_added` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
+  `url_date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-ALTER TABLE `biturl_data`
+ALTER TABLE `curltab_data`
   ADD PRIMARY KEY (`id`);
 
-ALTER TABLE `biturl_data`
+ALTER TABLE `curltab_data`
   MODIFY `id` int(9) NOT NULL AUTO_INCREMENT;
 COMMIT;
